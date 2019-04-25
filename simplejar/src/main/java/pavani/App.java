f661e3c30c8843e5ad1297fccc73efa60610f62a
pavani <avvaru.pavani@gmail.com>
2019-04-25 13:58:38 +0000

    new files added

---
+++ simplejar/src/main/java/pavani/App.java
@@ -0,0 +1,13 @@
+package pavani;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! And welcome to pavani's world of devops"  );
+    }
+}
